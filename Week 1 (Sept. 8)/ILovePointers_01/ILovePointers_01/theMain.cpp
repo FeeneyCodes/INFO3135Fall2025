65831d8440cc604f9756e023ca2a9e8f77b3441a
michaelfeeney <michaelwcfeeney@gmail.com>
2025-09-08 09:43:50 -0400

    More pointer stuff

--- Week 1 (Sept. 8)/ILovePointers_01/ILovePointers_01/theMain.cpp
+++ Week 1 (Sept. 8)/ILovePointers_01/ILovePointers_01/theMain.cpp
@@ -1,13 +1,28 @@
-
+#include <iostream>
 
 
 int main(int argc, char* argv[])
 {
 
+//	int x;
+//	x = 10;
+	int x = 10;		// two things
+	// Your mission, should you choose to accept it:
+	// Add 5 to this and output
+//
+//	x += 5;		//	x = x + 5;
+//	std::cout << "x = " << x << std::endl;
 
+	// Get the address of X and store it in a pointer
+	int* pX;			// Allocate a spot to hold a pointer
+	pX = &x;		// "address of"
+	// 1006
+	
+	// "dereference" the pointer
+	*pX += 5;	// Add 5 to "x" in this case
 
+	std::cout << x << std::endl;
 
-
-
+		
 	return 0;
 }
