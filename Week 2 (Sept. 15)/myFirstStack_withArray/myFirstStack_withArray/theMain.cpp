30cb41ac75c066d86404b62f5c78abfa5e27a260
michaelfeeney <michaelwcfeeney@gmail.com>
2025-09-15 09:38:57 -0400

    What fun it is to make a stack

--- Week 2 (Sept. 15)/myFirstStack_withArray/myFirstStack_withArray/theMain.cpp
+++ Week 2 (Sept. 15)/myFirstStack_withArray/myFirstStack_withArray/theMain.cpp
@@ -1,11 +1,31 @@
 #include "cStackArray.h"
+#include <fstream>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+	std::ifstream songFile("SomeSongs.txt");
+	if (!songFile.is_open())
+	{
+		std::cout << "Can't find file" << std::endl;
+		return -1;
+	}
 
+//	cStackArray myStack;
+//	myStack.pop();
+	
+	cStackArray* pMyStack = new cStackArray();
 
+	std::string songName;
+	while (songFile >> songName)
+	{
+//		std::cout << songName << std::endl;
+		pMyStack->push();
+	}
 
+	// Print out the data using pop()
 
+	delete pMyStack;
 
 	return 0;
 }
