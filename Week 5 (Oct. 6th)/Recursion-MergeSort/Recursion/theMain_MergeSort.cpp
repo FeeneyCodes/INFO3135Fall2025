288f40cae922575096877af3840a624fcd8a1b09
michaelfeeney <michaelwcfeeney@gmail.com>
2025-10-20 10:17:48 -0400

    Merge sort starter

---
+++ Week 5 (Oct. 6th)/Recursion-MergeSort/Recursion/theMain_MergeSort.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <string>
+
+
+int main()
+{
+	std::vector<std::string> vecNames
+	{ "Michael", "Mary", "Patricia", "Carol", "Betty",
+		"Barbara", "Dorothy", "Joan", "Patricia", "Margaret",
+		"Helen", "Shirley", "Doris", "Ruth", "Joyce", "Nancy"
+		"Virginia", "Jean", "Marilyn", "Elizabeth", "Dolores",
+		"Frances", "Lois", "Alice", "Norma", "Evelyn", "Anna" };
+
+
+
+	return 0;
+}
