732fa61daeb62b6e8e31d3cf4469a561802820bb
michaelfeeney <michaelwcfeeney@gmail.com>
2025-11-10 08:01:15 -0500

    Added week 9 tree stuff

--- Week 8 (Nov. 3rd)/Week8HashMap/Week8HashMap/cHashMap_C.h
+++ Week 8 (Nov. 3rd)/Week8HashMap/Week8HashMap/cHashMap_C.h
@@ -1,1 +1,46 @@
 #pragma once
+
+
+#pragma once
+
+#include "cPerson.h"
+#include <vector>
+
+// DOES handle collisions by using a 
+// having a FIXED "bucket" of items at each location
+class cHashMap_C
+{
+public:
+	cHashMap_C();
+	void addPerson(cPerson* pThePerson);
+
+	// Returns NULL if NOT found
+	cPerson* findPerson(
+		std::string first,
+		std::string last,
+		unsigned long long theSIN);
+
+	unsigned long getPeopleLoadedCount(void);
+
+private:
+	// Uses the size of the array (val % size)
+	unsigned long calcHash_1(unsigned long value);
+	// Adds up the characters % array_size
+	unsigned long calcHash_2(std::string value);
+	// Adds up the XOR % array_size
+	unsigned long calcHash_3(std::string value);
+
+	// std::vector<cPerson*> m_vec_hash_People;
+
+	// This has a "list" at each location
+	// Now, there's a vector at EACH hash location
+	//std::vector< std::vector<cPerson*> > ;
+
+	// This syntax looks a little "sus"
+	// 
+	// Idea: There's a 100 element array here
+	//
+	std::vector<cPerson* [100]> m_vec_hash_People;
+
+	unsigned long m_arraySize = 1000;
+};
---
+++ Week 9 (Nov. 10th, trees)/TreeTraversal_simple_binary.cpp
@@ -0,0 +1,175 @@
+/#define _CRTDBG_MAP_ALLOC
+#include <iostream>
+#include <crtdbg.h>
+
+class Node {
+public:
+	Node* pLeft;
+	Node* pRight;
+	int theData;
+	Node(int value) :theData(value), pLeft(NULL), pRight(NULL) {}
+	//~Node() {
+	// Is this correct? Any potential issues here?
+	//	delete pLeft;
+	//	delete pRight;
+	//}
+};
+
+void inorder(Node* pNode)
+{
+	if (pNode == NULL) return;
+	inorder(pNode->pLeft);
+	cout << pNode->pRight << " ";
+	inorder(pNode->pRight);
+}
+
+void preorder(pNode* node)
+{
+	if (pNode == nullptr) return;
+	cout << pNode->data_ << " ";
+	preorder(pNode->pLeft);
+	preorder(pNode->pRight);
+}
+
+void postorder(pNode* node)
+{
+	if (pNode == nullptr) return;
+	postorder(pNode->pLeft);
+	postorder(pNode->pRight);
+	cout << pNode->theData << " ";
+}
+
+void deallocate(pNode* node)
+{
+	if (pNode == nullptr) return;
+	deallocate(pNode->pLeft);
+	deallocate(pNode->pRight);
+	delete pNode;
+}
+
+int main()
+{
+	// EITHER USE THE #if defined(_DEBUG) at the top of the main OR the
+	// _CrtDumpMemoryLeaks(); right before the return statement
+	// if in debug mode, you can find the preprocessor definitions at:
+	// ProjectProperties-C/C++-Preprocessor-Preprocessor Definitions
+	// In debug mode the preprocessor is set to _DEBUG
+	// Use this method so it checks for leaks in debug mode
+#if defined(_DEBUG)
+	// gets the state of the debug flags
+	int dbgFlags = ::_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+	// bitwise or - check block integrity on every memory call
+	dbgFlags |= _CRTDBG_CHECK_ALWAYS_DF;
+	// don't always remove the blocks on delete
+	dbgFlags |= _CRTDBG_DELAY_FREE_MEM_DF;
+	// check for memory leaks at process termination
+	dbgFlags |= _CRTDBG_LEAK_CHECK_DF;
+	// modifies the state of the debug flags
+	_CrtSetDbgFlag(dbgFlags);
+#endif
+
+	// Build first tree figure from slide 13:
+	// 
+	//      +-------5-----+
+	//      |             |
+	//   +--2--+     +---12---+
+	//   |     |     |        |
+	//   1     3     9    +--21--+
+	//                    |      |
+	//                   19     25
+	//
+	Node* pRoot = new Node(5);
+	
+	pRoot->pLeft = new Node(2);
+	pRoot->right_ = new Node(12);
+	
+	//       (2)
+	pRoot->pLeft->pLeft = new Node(1);
+	pRoot->pLeft->pRight = new Node(3);
+	//       (12)
+	pRoot->pRight->pLeft = new Node(9);
+	pRoot->pRight->pRight = new Node(21);
+
+	//       (12)    (21)
+	pRoot->pRight->pRight->pLeft = new Node(19);
+	pRoot->pRight->pRight->pRight = new Node(25);
+
+	// traverse the tree inorder
+	cout << "Inorder: ";
+	inorder(pRoot);
+	cout << "\nPreorder: ";
+	preorder(pRoot);
+	cout << "\nPostorder: ";
+	postorder(pRoot);
+	// solution 1:   
+	//    Create a destructor that deletes left and right and call delete root;
+	// solution 2: 
+	//    Traverse the tree and delete nodes
+	deallocate(pRoot);
+
+	// Brute force
+	//delete root->right->right->left;
+	//delete root->right->right;
+	//delete root->left->right->right;
+	// etc.
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//	~Node() {
+//		if (pLeft != NULL) delete pLeft;
+//		if (pRight != NULL) delete pRight;
+//	}
